cf9c5084fd1a15f4f6146d99b261185b06f3c1df
SofiaMaida <54561075+SofiaMaida@users.noreply.github.com>
2025-06-23 19:58:06 -0300

    arreglo de archivos

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,8 +1,8 @@
 // File: C:\Users\Sofia\OneDrive\Escritorio\pedido360\pedido360-pp3\src\app\layout.tsx
-import * as entry from '../../../src/app/layout.js'
+import * as entry from '../../../backend/src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../src/app/layout.js')
+type TEntry = typeof import('../../../backend/src/app/layout.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,8 +1,8 @@
 // File: C:\Users\Sofia\OneDrive\Escritorio\pedido360\pedido360-pp3\src\app\page.tsx
-import * as entry from '../../../src/app/page.js'
+import * as entry from '../../../backend/src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../src/app/page.js')
+type TEntry = typeof import('../../../backend/src/app/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
